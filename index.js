a90ae76a985ef09ef8d8fd1e6c5140ddecb70282
ArshiyaHafis <arshiyapadiyath@gmail.com>
2023-01-07 21:55:11 +0530

    index.js commit

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const mysql = require('mysql');
+const bodyparser = require('body-parser');
 const app = express();
+app.use(bodyparser.json());
 // set your preferred server port
 const port = 3080;
 // root endpoint response
@@ -27,42 +29,109 @@ app.listen(port, () => {
   console.log(`Your server ⚡ is running 🏃‍♂️ on http://localhost:${port}`);
 });
 
-app.get('/music' , (req, res) => {
+
+//GET REQUEST
+//MUSIC TABLE 
+app.get('/playlist' , (req, res) => {
   	sqlconnect.query('SELECT * FROM playlists', (err, rows, fields) => {
 		if (!err)
 			res.send(rows);
 		else
 			console.log(err);
 	})
-	} );
+});
 
-  
-app.get('/music/:id' , (req, res) => {
+
+//SONGS TABLE
+app.get('/songs', (req, res) => {
+	sqlconnect.query('SELECT * FROM songs', (err, rows, fields) => {
+	  if (!err)
+		  res.send(rows);
+	  else
+		  console.log(err);
+  })
+});
+
+//GET ID
+//playlists table
+app.get('/playlists/:id' , (req, res) => {
     sqlconnect.query('SELECT * FROM playlists WHERE ID = ?',[req.params.id], (err, rows, fields) => {
 		if (!err)
 		res.send(rows);
 		else
 		console.log(err);
 		})
-    });
-app.post('/music', (req, res) => {
-	let name = req.body;
-	var sql = "SET @ID = ?;SET @NAME = ?;SET @SONGS = ?;CALL musicaddoredit(@ID,@NAME,@SONGS);";
-	sqlconnect.query(sql, [name.ID, name.NAME, name.SONGS], (err, rows, fields) => {
-	if (!err)
-		rows.forEach(element => {
-		if(element.constructor == Array)
-		res.send('New sONG ID : '+ element[0].learner_id);
-		});
-	else
+});
+
+//SONGS TABLE
+app.get('/songs/:id' , (req, res) => {
+    sqlconnect.query('SELECT * FROM songs WHERE ID = ?',[req.params.id], (err, rows, fields) => {
+		if (!err)
+		res.send(rows);
+		else
 		console.log(err);
-	})
-	});
-app.delete('/music/:id', (req, res) => {
+		})
+});
+
+
+//POST REQUEST
+
+// app.post('/playlist', (req, res) => {
+// 	const name = req.body.name;
+// 	const  id = req.body.ID;
+// 	const songs = req.body.songs;
+// 	var sql = "SET @ID = ?;SET @NAME = ?;SET @SONGS = ?;CALL musicaddoredit(@ID,@NAME,@SONGS);";
+// 	sqlconnect.query(sql, [name.Id, name.NAME, name.SONGS], (err, rows, fields) => {
+// 	if (!err)
+// 		rows.forEach(element => {
+// 		if(element.constructor == Array)
+// 		res.send('New sONG ID : '+ element[0].id);
+// 		});
+// 	else
+// 		console.log(err);
+// 	})
+// });
+
+
+//CREATE POST REQUEST
+
+app.post('/playlist', (req, res) => {
+	const data = req.body;
+	var sql = "INSERT INTO PLAYLISTS SET?";
+	sqlconnect.query(sql, data, (err, rows, fields) => {
+		if (err)
+			console.log(err);
+		});
+});
+
+// SONGS TABLE
+app.post('/song', (req, res) => {
+	const data = req.body;
+	var sql = "INSERT INTO SONGS SET?";
+	sqlconnect.query(sql, data, (err, rows, fields) => {
+		if (err)
+			console.log(err);
+		});
+});
+
+
+//DELETE REQUEST
+//PLAYLISTS TABLE
+app.delete('/playlist/:id', (req, res) => {
 	sqlconnect.query('DELETE FROM playlists WHERE ID = ?', [req.params.id], (err, rows, fields) => {
 	if (!err)
 		res.send('Learner Record deleted successfully.');
 	else
 		console.log(err);
 	})
-	});
+});
+
+//SONGS TABLE
+app.delete('/songs/:id', (req, res) => {
+	sqlconnect.query('DELETE FROM songs WHERE ID = ?', [req.params.id], (err, rows, fields) => {
+	if (!err)
+		res.send('Learner Record deleted successfully.');
+	else
+		console.log(err);
+	})
+});
